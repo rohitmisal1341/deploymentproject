3051961d785b1e2a26943c2e8a1a22556d214a08
rohitmisal1341 <rohitmisal1341@gmail.com>
2022-07-01 09:48:43 +0530

    first commit

---
+++ src/main/java/com/learn/deploy/controller/Mycontroller.java
@@ -0,0 +1,21 @@
+package com.learn.deploy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Mycontroller {
+
+	@GetMapping("/")
+	public String home() {
+		return "home page";
+	}
+	
+	@GetMapping("/about")
+	public String about() {
+		return "about page";
+		
+	}
+	
+	
+}
---
+++ src/test/java/com/learn/deploy/JenkinsDeployPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDeployPrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
